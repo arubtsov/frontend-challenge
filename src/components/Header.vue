dfff90d6ce518127f639307dd399fa60a8d339c0
Rb3As <arseniy.rubtsov@gmail.com>
2019-02-03 19:22:47 +0300

    fetch and upload buttons have been added into Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,20 @@
 <template>
   <header>
     <h1>Transcriptions</h1>
+    <Button src="upload.svg"/>
+    <Button src="fetch-document.svg"/>    
   </header>
 </template>
 
 <script>
+import Button from './Button';
+
 export default {
-  name: "Header"
+  name: "Header",
+
+  components: {
+      Button
+  }
 };
 </script>
 
