01695c7b82c26437e9a9abcffd23abb8832c2677
Rb3As <arseniy.rubtsov@gmail.com>
2019-02-03 18:26:01 +0300

    TranscriptionsList component has been created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div id="app">
     <Header/>
+    <TranscriptionsList v-bind:items="transcriptions"/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import TranscriptionsList from "./components/TranscriptionsList.vue";
 
 export default {
   name: "app",
+
   components: {
-    Header
+    Header,
+    TranscriptionsList
+  },
+
+  data() {
+    return {
+      transcriptions: [
+        {
+          id: 0,
+          voice: "Voice 1",
+          text: "some text"
+        },
+        {
+          id: 1,
+          voice: "Voice 2",
+          text: "some other text"
+        }
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/TranscriptionsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="item in items" v-bind:key="item.id">
+      <p>{{item.voice}}</p>
+      <p>{{item.text}}</p>
+    </li>
+  </ul>
+</template>
+
+
+<script>
+export default {
+  name: "TranscriptionsList",
+  props: {
+    items: Array
+  }
+};
+</script>
+
+<style>
+</style>
