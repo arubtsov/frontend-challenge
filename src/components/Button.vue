3c026f67f62f4bc32c3578f127116a99fc391aef
Rb3As <arseniy.rubtsov@gmail.com>
2019-02-10 18:14:31 +0300

    enter key press handling in Button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,21 @@
 <template>
-  <button>
-    <img :src="require(`@/assets/${src}`)">
+  <button @keydown.enter.native="triggerClick">
+    <img :src="require(`@/assets/${src}`)">  
   </button>
 </template>
 
 <script>
 export default {
   name: "Button",
+  
   props: {
     src: String
+  },
+
+  methods: {
+    triggerClick() {
+      this.$refs.sendReply.click();
+    }
   }
 };
 </script>
-
