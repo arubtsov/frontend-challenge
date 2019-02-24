a758e035dedb9cd88412c07f0d24a921dc18f2cd
Rb3As <arseniy.rubtsov@gmail.com>
2019-02-24 19:53:28 +0300

    MainColumn component has been added

---
+++ src/components/MainColumn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="className">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainCoulumn",
+
+  props: {
+    className: String
+  }
+};
+</script>
+
+<style scoped>
+div {
+  width: 100%;
+  max-width: 738px;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+</style>
