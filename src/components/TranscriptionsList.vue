6b5fff77dba2c03957a1523b43c9c7a1948f3e6d
Rb3As <arseniy.rubtsov@gmail.com>
2019-02-24 19:58:04 +0300

    TranscriptionsList component's styles

--- src/components/TranscriptionsList.vue
+++ src/components/TranscriptionsList.vue
@@ -1,24 +1,27 @@
 <template>
   <main>
-    <ul>
-      <Transcription v-for="item in transcriptions" :key="item.id" :data="item"/>
-    </ul>
-    <Button iconFile="add-row.svg" @click.native="add"/>
+    <MainColumn class="container flex column centred">
+      <ul v-if="transcriptions.length">
+        <Transcription v-for="item in transcriptions" :key="item.id" :data="item"/>
+      </ul>
+      <Button iconFile="add-row.svg" @click.native="add"/>
+    </MainColumn>
   </main>
 </template>
 
 
 <script>
-import { ADD_TRANSCRIPTION } from '../store/mutatuion-types.js';
-import { mapState, mapMutations } from 'vuex';
+import { ADD_TRANSCRIPTION } from "../store/mutatuion-types.js";
+import { mapState, mapMutations } from "vuex";
 
-import Transcription from './Transcription';
+import Transcription from "./Transcription";
 import Button from "./Button";
+import MainColumn from "./MainColumn";
 
 export default {
   name: "TranscriptionsList",
 
-  computed: mapState(['transcriptions']),
+  computed: mapState(["transcriptions"]),
 
   methods: {
     ...mapMutations({ add: ADD_TRANSCRIPTION })
@@ -26,10 +29,37 @@ export default {
 
   components: {
     Transcription,
-    Button
+    Button,
+    MainColumn
   }
 };
 </script>
 
-<style>
+<style scoped>
+main {
+  height: 100%;
+  background-color: #f6f7f8;
+}
+
+.container > * {
+  margin-top: 24px;
+}
+
+ul {
+  list-style: none;
+  width: 100%;  
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eaedef;
+  box-sizing: border-box;
+  background-color: white;
+}
+
+li {
+  padding: 24px;
+}
+
+li:not(:last-child) {
+  border-bottom: 1px solid #eaedef;
+}
 </style>
