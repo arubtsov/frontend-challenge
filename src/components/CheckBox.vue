e7b5145dc26a1a9293f9048e925c7a83f638bdb3
Rb3As <arseniy.rubtsov@gmail.com>
2019-02-24 20:00:54 +0300

    CheckBox styles

--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -1,5 +1,9 @@
 <template>
-  <input type="checkbox" :checked="checked">
+  <label class="flex centred">
+    <input type="checkbox" :checked="checked" :disabled="disabled">
+    <div/>
+    <span v-if="label">{{label}}</span>
+  </label>
 </template>
 
 <script>
@@ -7,10 +11,53 @@ export default {
   name: "CheckBox",
 
   props: {
-    checked: Boolean
+    checked: {
+      type: Boolean,
+      required: true
+    },
+
+    label: {
+      type: String,
+      default: ""
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false
+    }
   }
 };
 </script>
 
 <style scoped>
+input, div {
+  width: 16px;
+  height: 16px;
+}
+
+input {
+  margin: 0;
+  position: absolute;
+}
+
+input:focus {
+  outline-offset: 5px !important;
+}
+
+div {  
+  border-radius: 2px;
+  box-sizing: border-box;
+  border:  2px solid #859EFF;
+  background-color: white;
+  z-index: 1;
+}
+
+input:checked ~ div {
+  background-image: url('../assets/check-box.svg');
+  border: none;
+}
+
+span {
+  margin-left: 0.5em;
+}
 </style>
