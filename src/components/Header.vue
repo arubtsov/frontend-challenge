bab0372109b32674e2bf878c2776f01480070b3a
Rb3As <arseniy.rubtsov@gmail.com>
2019-02-24 19:56:30 +0300

    Header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,26 @@
 <template>
-  <header>
-    <h1>Transcriptions</h1>
-    <Button iconFile="upload.svg" @click.native="upload()"/>
-    <Button iconFile="fetch-document.svg" @click.native="fetch()"/>
+  <header class="flex centred">
+    <MainColumn class="container flex centred">
+      <h1>Transcriptions</h1>
+      <Button iconFile="upload.svg" @click.native="upload()"/>
+      <Button iconFile="fetch-document.svg" @click.native="fetch()"/>
+    </MainColumn>
   </header>
 </template>
 
 <script>
-import { FETCH_DATA, UPLOAD_DATA } from '../store/action-types.js';
-import { mapActions } from 'vuex';
+import { FETCH_DATA, UPLOAD_DATA } from "../store/action-types.js";
+import { mapActions } from "vuex";
 
 import Button from "./Button";
+import MainColumn from "./MainColumn";
 
 export default {
   name: "Header",
 
   components: {
-    Button
+    Button,
+    MainColumn
   },
 
   methods: {
@@ -25,5 +29,30 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+header {
+  z-index: 1;
+  box-shadow: 0px 1px 2px 0px #bcbdbe, 0px 1px 3px 0px #bcbdbe;
+  height: 69px;
+}
+
+h1 {
+  font-family: "Montserrat", sans-serif;
+  font-size: 18px;
+  font-weight: 500;
+  color: #414c5e;
+}
+
+.container {
+  justify-content: space-between;
+  padding: 0 24px;
+}
+
+h1 {
+  margin-right: auto;
+}
+
+button {
+  margin-left: 24px;
+}
 </style>
